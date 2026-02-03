2eac82cb5c0568a4b7b368786e2b320c0673c65f
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-03 12:04:42 -0300

    refactor: movendo conteudo para componentes tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
@@ -6,17 +6,13 @@ import "./styles/theme.css";
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo{" "}
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-        praesentium, totam minima nostrum sint at. Eum debitis distinctio optio
-        facilis cumque eius voluptatem! Animi velit aliquid sequi non eum sit.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
