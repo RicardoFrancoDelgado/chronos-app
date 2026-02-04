a6e894d6fcf6b839d98864980fb93f3e47809c79
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-04 15:50:01 -0300

    feat: criando a estrutura do formulário para iniciar a task

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,28 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className="form">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input type="text" id="input" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+
+          <div className="formRow">
+            <p>ciclos:</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
