1da493e795261034c26103f7a931af7f2986ad97
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-06 17:42:30 -0300

    feat: criando modelos de task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Home } from "./pages/Home";
 // import { AboutPomodoro } from "./pages/AboutPomodoro";
-import { NotFound } from "./pages/NotFound";
+// import { NotFound } from "./pages/NotFound";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
-  return <NotFound />;
+  return <Home />;
 }
