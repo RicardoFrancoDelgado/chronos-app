4829fcc7c24c36a82223d8e7b908082299ba6617
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-06 22:00:04 -0300

    feat: compartilhando estados por props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
 import { Home } from "./pages/Home";
 // import { AboutPomodoro } from "./pages/AboutPomodoro";
 // import { NotFound } from "./pages/NotFound";
 import "./styles/global.css";
 import "./styles/theme.css";
 
+const initialTask: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialTask);
+
+  console.log(state);
+
+  return <Home state={state} setState={setState} />;
 }
