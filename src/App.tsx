1fa227041de7808575d225bdb3fc5659fbebb68c
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-09 15:40:46 -0300

    feat: usando state + context no provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 // import { AboutPomodoro } from "./pages/AboutPomodoro";
 // import { NotFound } from "./pages/NotFound";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
+
 import { Home } from "./pages/Home";
 import "./styles/global.css";
 import "./styles/theme.css";
 import { TaskContextProvider } from "./contexts/TaskContext";
 
-const initialTask: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export function App() {
-  const [state, setState] = useState(initialTask);
-
   return (
     <TaskContextProvider>
       <Home />
--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,6 +1,7 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 
+// estado inicial da task
 const initialState: TaskStateModel = {
   tasks: [],
   secondsRemaining: 0,
@@ -14,33 +15,40 @@ const initialState: TaskStateModel = {
   },
 };
 
+// tipagem da task
 type TaskContextProps = {
   state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 };
 
+// contexto da task recebendo um estado inicial e uma função de callback
 const initialContextValue = {
   state: initialState,
   setState: () => {},
 };
 
+// primeiro export (fastreload vai reclamar)
 export const TaskContext = createContext<TaskContextProps>(initialContextValue);
 
 // Provider personalizado
-
+// tipo do provider
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
+// componente do provider
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, setState] = useState(initialState);
+
   return (
-    <TaskContext.Provider value={{ ...initialContextValue }}>
+    <TaskContext.Provider value={{ state, setState }}>
       {children}
     </TaskContext.Provider>
   );
 }
 
-// hook personalizado
+// hook personalizado -> fast refresh também vai reclamar
+// hook personalizado para o contador
 export function useTaskContext() {
   return useContext(TaskContext);
 }
