78dc3b7ca297c3e33e312e1600b799688c5161bf
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-09 17:02:04 -0300

    feat: capturando o submit e tirando o comportamento padrão

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,26 +2,15 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
-
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-
-  function handleClick() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: "21:00",
-      };
-    });
+  function handleCreateNewTask(ev: React.SubmitEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    console.log("tome");
   }
 
   return (
-    <form className="form">
-      <button onClick={handleClick} type="button">
-        clicar
-      </button>
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <DefaultInput
           labelText="task"
