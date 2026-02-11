c8b821d6556f5f93e15636dcce286b346c6e760e
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-11 16:28:53 -0300

    feat: adicionando validação na criação de uma task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,14 +3,49 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
   const taskInputName = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(ev: React.SubmitEvent<HTMLFormElement>) {
     ev.preventDefault();
 
-    console.log("tome", taskInputName.current.value);
+    if (taskInputName.current === null) return;
+
+    const taskName = taskInputName.current.value.trim();
+
+    if (!taskName) {
+      alert("Insira o nome da task");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1, //TODO
+      type: "workTime", //TODO
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((currentTask) => {
+      return {
+        ...currentTask,
+        config: { ...currentTask.config },
+        activeTask: newTask,
+        currentCycle: 1, //TODO
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00", //TODO
+        tasks: [...currentTask.tasks, newTask],
+      };
+    });
   }
 
   return (
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,6 +1,6 @@
 // Provider personalizado
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -12,6 +12,10 @@ type TaskContextProviderProps = {
 // componente do provider
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
